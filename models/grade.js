7591e94eee1f30680b14a294acd0de6444a8f805
Fisiha <fishbrnd@gmail.com>
2026-02-02 14:42:14 +0300

    adding a grade model and make an grade route.js file and an ejs file for it

--- models/grade.js
+++ models/grade.js
@@ -5,9 +5,11 @@ const mongoose = require('mongoose');
 
 const gradeSchema = new mongoose.Schema({
   student: { type: mongoose.Schema.Types.ObjectId, ref: 'Student', required: true },
-  course: { type: mongoose.Schema.Types.ObjectId, ref: 'AddCourse', required: true },
+  course: { type: mongoose.Schema.Types.ObjectId, ref: 'course', required: true },
   grade: { type: String, required: true },
-  semester: String
+   semester: String,
+
 });
+gradeSchema.index({ student: 1, course: 1 }, { unique: true });
 
 module.exports = mongoose.model('Grade', gradeSchema);
--- route/GRADESUBMITION.route.js
+++ route/GRADESUBMITION.route.js
@@ -3,28 +3,56 @@ const router = express.Router();
 const dept = require('../models/newdpart')
 const stud = require('../models/regstud');
 const cor = require('../models/addcourse');
-const grade = require('../models/grade');
+const Grade = require('../models/grade');
 
 
 //getting a grade submition page 
 router.get('/:deptId/:studId',async(req,res)=>{
     const {deptId,studId} = req.params;
     const deptData = await dept.findById(deptId);
-    const course = await cor.find({dept:deptId});
-    const studd = await stud.findById(studId)
-    res.render('GRADESUBMITION',{dept:deptData,stud:studd,course})
+   // const course = await cor.find({dept:deptId});
+    const studd = await stud.findById(studId).populate('course');
+    res.render('GRADESUBMITION',{dept:deptData,stud:studd,course:studd.course})
 })
 
 //recived a value from grade submition page and saving to database 
 router.post('/:deptId/:studId',async(req,res)=>{
 
-    const {deptId,studId,courseId} = req.params;
-    const deptData = await dept.findById(deptId);
-    const stude = await stud.findById(studId);
-    const course = await course.findById(courseId);
-    const grade = req.body;
+    const {deptId,studId} = req.params;
 
-    await grade.save();
-    res.send('student grade is registerd')
-}) 
+    const {grade,courseId} = req.body;
+try{
+    const newGrade = new Grade({
+        student : studId,
+        dept    : deptId,
+        course : courseId,
+        grade  
+    });
+    await newGrade.save();
+    res.send('grade is submitted');
+}catch(err){
+    if(err.code == 11000){
+        res.send('this course already has grade')
+    }else{
+        console.log(err)
+        res.send('somthing is wrong')
+    }
+}
+    }) 
+router.get('/:deptId/:studId/grades',async(req,res)=>{
+      const {deptId,studId} = req.params;
+      
+         try{
+            const stude = await stud.findById(studId);
+         const deptData = await dept.findById(deptId);
+        const gradee = await Grade.find({student:studId}).populate('course');
+        
+         res.render('grades',{stude,deptData,gradee})
+         }catch(error){
+            console.log(error)
+            res.send('could not show up ')
+         }
+})
+   
+
 module.exports = router;
